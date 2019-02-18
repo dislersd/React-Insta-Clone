40a85b85e54ea914862f0454a9bd043b9d51d460
dislersd <dislersd@gmail.com>
2019-02-18 15:21:32 -0800

    mapped through and rendered Comment components in CommentSection component

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Comment = props => {
-  return (
-    <div className="comment">
-      <p>{props.comments.username}</p>
+const Comment = props => (
+  <div className='comment-container'>
+  {props.data.map( comment => 
+    <div className='comment'>
+      <span>{comment.username}</span>
+      {comment.text}
     </div>
-  );
-}
+  )}
+  </div>
+);
 
 export default Comment;
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import Comment from './Comment';
+import React from "react";
+import Comment from "./Comment";
+import './Comment.css'
 
 class CommentSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       dummyData: this.props.dummyData
-    }
+    };
   }
 
   render() {
     return (
-      <div className='comment-section-wrapper'>
-        <div className='like-and-comment'></div>
-        <div className="likes-counter"></div>
+      <div className="comment-section-wrapper">
+        <div className="like-and-comment" />
+        <div className="likes-counter" />
         <div className="comments">
-          {this.state.dummyData.map( dataObject => 
-            <Comment 
-            comments={dataObject.comments}
-
-            />
-          )}
-          
+          <Comment data={this.state.dummyData.comments} />
         </div>
         <input className="add-comment" />
       </div>
